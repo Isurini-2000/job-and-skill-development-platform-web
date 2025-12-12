eebc9791cbacaf7195697847742ff73aff2afbf0
Isurini-2000 <isurinisanka3@gmail.com>
2025-12-12 22:42:32 +0530

    Create TrainerDashboard page

---
+++ src/pages/TrainerDashboard/TrainerDashboard.jsx
@@ -0,0 +1,139 @@
+import React from "react";
+import { Box, Typography, Card, Stack } from "@mui/material";
+import { PieChart, Pie, Tooltip, Cell, Legend, ResponsiveContainer } from "recharts";
+
+export default function TrainerDashboard() {
+    const courseStats = {
+        totalCourses: 25,
+        activeCourses: 12,
+    };
+
+    const entrollsData = [
+        { name: "Applied", value: 233 },
+        { name: "Selected", value: 159 },
+        { name: "Rejected", value: 64 },
+    ];
+
+    const COLORS = ["#044315ff ", "#FB8C00 ", "#FF0000 "];
+
+    return (
+        <Box
+            sx={{
+                p: 4,
+                backgroundColor: "#e8f5e9",
+                minHeight: "100vh",
+            }}
+        >
+
+            <Typography
+                variant="h4"
+                sx={{
+                    fontWeight: "bold",
+                    mb: 4,
+                    color: "#2e7d32",
+                }}
+            >
+                Dashboard
+            </Typography>
+
+
+            <Stack direction="row" justifyContent="space-evenly" sx={{ mb: 4, width: '100%' }} >
+                <Card
+                    sx={{
+                        p: 4,
+                        borderLeft: "10px solid #2e7d32",
+                        minWidth: 350,
+                        height: 200,
+                        borderRadius: 3,
+                        boxShadow: 3,
+                        display: "flex",
+                        flexDirection: "column",
+                        backgroundColor: "#1fd655",
+                    }}
+                >
+                    <Typography variant="h4" fontWeight="bold">
+                        Total Courses
+                    </Typography>
+
+                    <Typography
+                        variant="h1"
+                        fontWeight="bold"
+                        sx={{ textAlign: "center", width: "100%" }}
+                    >
+                        12
+                    </Typography>
+                </Card>
+
+
+                <Card
+                    sx={{
+                        p: 4,
+                        borderLeft: "10px solid #2e7d32",
+                        minWidth: 350,
+                        height: 200,
+                        borderRadius: 3,
+                        boxShadow: 3,
+                        display: "flex",
+                        flexDirection: "column",
+                        backgroundColor: "#5ced73",
+                    }}
+                >
+                    <Typography variant="h4" fontWeight="bold">
+                        Active Courses
+                    </Typography>
+
+                    <Typography
+                        variant="h1"
+                        fontWeight="bold"
+                        sx={{ textAlign: "center", width: "100%" }}
+                    >
+                        8
+                    </Typography>
+                </Card>
+            </Stack>
+
+
+            <Card
+                sx={{
+                    mt: 4,
+                    p: 3,
+                    borderRadius: 3,
+                    background: "#ffffff",
+                    boxShadow: 3,
+                }}
+            >
+                <Typography variant="h6" sx={{ mb: 2, color: "#07560bff", fontWeight: "bold" }}>
+                    Entrolls Overview
+                </Typography>
+
+                <Box sx={{ width: "100%", height: 400 }}>
+                    <ResponsiveContainer>
+                        <PieChart>
+                            <Pie
+                                data={entrollsData}
+                                cx="30%"
+                                cy="50%"
+                                outerRadius={150}
+                                dataKey="value"
+                                label
+                            >
+                                {entrollsData.map((entry, index) => (
+                                    <Cell key={index} fill={COLORS[index]} />
+                                ))}
+                            </Pie>
+                            <Tooltip />
+                            <Legend
+                                layout="vertical"
+                                align="center"
+                                verticalAlign="middle"
+                                iconType="circle"
+                                iconSize={18}
+                                wrapperStyle={{ lineHeight: '3em', }}
+                            />
+                        </PieChart>
+                    </ResponsiveContainer>
+                </Box>
+            </Card>
+        </Box>
+    );
+}
